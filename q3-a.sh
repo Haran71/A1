7267b13d5528bdc7a20546e7994e2e7c9166d3c7
Haran Raajesh <haran@Harans-MacBook-Pro.local>
2022-04-19 16:25:46 +0530

    removed q3-a.sh

--- q3-a.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-clear
-wc -c $1 | awk '{print $1}'
--- q3-e.sh
+++ q3-e.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 clear
 grep -wo "[[:alnum:]]\+" $1 > temp1.txt
-cat -n temp1.txt | sort -k2 | uniq -cdf1 | sort -nk2 > temp.txt 
+cat -n temp1.txt | sort -k2 | uniq -cdf1 > temp3.txt
+cat temp3.txt | sort -nk2 > temp.txt 
 while read line
 do
   echo "$(echo $line | awk '{print "Word: " $3 " Count of repition: " $1}')"
